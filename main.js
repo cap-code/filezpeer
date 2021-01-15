a2e741d9adf9ba7348763669e137a196870a0ef5
Advaith s <33504045+cap-code@users.noreply.github.com>
2021-01-15 10:07:46 +0530

    error

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ function createPeer(){
             file = new File([file],text.name,{lastModified: new Date().getTime(),type:text.ext});
             console.log(text.ext);
             console.log('received',file);
-                const p -document.createElement('p');
+                const p =document.createElement('p');
                 const a = document.createElement('a');
                 a.href = URL.createObjectURL(file);
                 a.textContent = "download";
